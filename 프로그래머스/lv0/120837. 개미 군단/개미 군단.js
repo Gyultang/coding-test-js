1d83ff7d8e618792212f7680f1e831a8574b1e86
KIM GYU RI <100915440+Gyultang@users.noreply.github.com>
2023-01-15 23:51:10 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -0,0 +1,8 @@
+function solution(hp) {
+    var answer = 0;
+    let general = Math.floor(hp/5);
+    let soldier = Math.floor((hp-(general*5))/3);
+    let ant = hp-(general*5)-(soldier*3);
+    
+    return general+soldier+ant;
+}
