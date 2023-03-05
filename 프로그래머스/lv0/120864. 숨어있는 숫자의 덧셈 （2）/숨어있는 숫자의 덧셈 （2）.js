8a94b039808bee1b42476577e98f1ed68acc906d
KIM GYU RI <100915440+Gyultang@users.noreply.github.com>
2023-03-05 18:30:40 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,11 +1,18 @@
 function solution(my_string) {
-    let res = 0;
-    let num = my_string.replace(/[^0-9]/g,"+");
-    let numArr=num.split("+")
-    for(e of numArr){
-        if(e !== ""){
-            res+=parseInt(e)
-        }
-    }
-    return  res;
+    let num = my_string.replace(/[^0-9]/g," ");
+    let numArr=num.split(" ")
+
+    return  numArr.map((e)=>e*1).reduce((a,b)=>a+b);
 }
+// 처음풀이
+// function solution(my_string) {
+//     let res = 0;
+//     let num = my_string.replace(/[^0-9]/g,"+");
+//     let numArr=num.split("+")
+//     for(e of numArr){
+//         if(e !== ""){
+//             res+=parseInt(e)
+//         }
+//     }
+//     return  res;
+// }
