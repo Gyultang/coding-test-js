823edc6e8fecfde62925e7a35e8079e3543ec6b6
KIM GYU RI <100915440+Gyultang@users.noreply.github.com>
2023-08-04 10:27:45 +0900

    [level 2] Title: 짝지어 제거하기, Time: 73.90 ms, Memory: 47 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,12 @@
+function solution(s)
+{
+    let stack=[];
+    for(let i=s.length-1;i>=0;i--){
+        if(s[i]===stack.at(-1)){
+            stack.pop();
+        }else{
+            stack.push(s[i])
+        }
+    }
+    return stack.length===0?1:0
+}
